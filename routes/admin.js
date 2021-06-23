34a50569e796ae8665cdf5781c208a870e20f9ed
unknown <denis.obraztsov.92@mail.ru>
2021-06-23 13:30:10 +0300

    Фетчи, ручки и переход на страницу одной заявки

--- routes/admin.js
+++ routes/admin.js
@@ -24,8 +24,39 @@ router.post('/signup', adminSignup);
 
 router.get('/requests', async (req, res) => {
   const request = await Request.find();
-  console.log(request);
-  res.render('requests', { admin: 'eeee' });
+  res.render('requests', { admin: 'eeee', request });
+});
+
+router.post('/requests', async (req, res) => {
+  const { search } = req.body;
+  const request = await Request.find({ status: 'выполнена' });
+  res.render('requests', { admin: 'eeee', request });
+});
+
+router.get('/requests/inprocess', async (req, res) => {
+  const request = await Request.find({ status: 'открыта' });
+  res.render('requests', { admin: 'eeee', request });
+});
+
+router.get('/requests/inwork', async (req, res) => {
+  const request = await Request.find({ status: 'в работе' });
+  res.render('requests', { admin: 'eeee', request });
+});
+
+router.get('/requests/canceled', async (req, res) => {
+  const request = await Request.find({ status: 'отменена' });
+  res.render('requests', { admin: 'eeee', request });
+});
+
+router.get('/requests/done', async (req, res) => {
+  const request = await Request.find({ status: 'выполнена' });
+  res.render('requests', { admin: 'eeee', request });
+});
+
+router.get('/requests/s/', (req, res) => {
+  // const { id } = req.params;
+  res.sendStatus(200);
+  // res.redirect(`/admin/requests/${id}`);
 });
 
 router.get('/requests/:idreq', showReq);
