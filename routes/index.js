f0f2b443991b8bf5fca3ea69d83517b2a62207ee
unknown <denis.obraztsov.92@mail.ru>
2021-06-27 08:21:33 +0300

    Финал

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const Request = require('../db/models/requestModel');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
-
 router.get('/', (req, res) => {
   res.render('index');
 });
-
-router.post('/apply', async (req, res) => {
+// var storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//       cb(null, 'uploads')
+//   },
+//   filename: (req, file, cb) => {
+//       cb(null, file.fieldname + '-' + Date.now())
+//   }
+// });
+// var upload = multer({ storage: storage });
+router.post('/apply', upload.single('file'), async (req, res) => {
   // console.log('Hello');
   try {
     console.log('Зашел в ручку');
     const {
-      name, companyName, phone, email, description, img, link,
+      name, companyName, phone, email, description, link,
     } = req.body;
+    console.log(req.file);
+  //   const img = {
+  //     data: await fs.readFile(path.join(__dirname + '/uploads/' + req.file.filename)),
+  //     contentType: 'image/png'
+  // };
     const newRequest = new Request({
-      name, companyName, phone, email, description, img, link,
+      name, companyName, phone, email, description, link,
     });
     await newRequest.save();
     // res.render('index');
@@ -29,5 +44,4 @@ router.post('/apply', async (req, res) => {
   
   res.send('База данных пополнена');
 });
-
 module.exports = router;
